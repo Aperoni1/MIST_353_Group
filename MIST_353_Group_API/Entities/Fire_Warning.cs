1584357ef0154d4f12bf964f8bbe77ce5d7fce97
Aperoni1 <111775696+Aperoni1@users.noreply.github.com>
2024-03-26 15:29:16 -0400

    Filled in the Fire_Warning Class

--- MIST_353_Group_API/Entities/Fire_Warning.cs
+++ MIST_353_Group_API/Entities/Fire_Warning.cs
@@ -1,6 +1,14 @@
-﻿namespace MIST_353_Group_API.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MIST_353_Group_API.Entities
 {
     public class Fire_Warning
     {
+        [Key]
+        public int WarningID { get; set; }
+        public DateTime TimeLastUpdated { get; set; }
+        public DateTime TimeFirstReported { get; set; }
+        public string Status { get; set; }
+        public int LocationID { get; set; }
     }
 }
