b262b32682b6d1ae224ae2786dc0332530ea8a28
jprocto3 <jprocto3@mix.wvu.edu>
2024-03-26 15:28:09 -0400

    Added Classses

---
+++ MIST_353_Group_API/Location.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MIST_353_Group_API
+{
+    public class Location
+    {
+        [Key]
+        public int LocationID { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Long { get; set; }
+        public string Lat { get; set; }
+        public string ParkName { get; set; }
+    }
+}
---
+++ MIST_353_Group_API/Weather.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MIST_353_Group_API
+{
+    public class Weather
+    {
+        [Key]
+        public int WeatherID { get; set; }
+        public int LocationID { get; set; }
+        public int FireWarningID { get; set; }  // Added Foreign Key
+        public decimal? Temperature { get; set; }
+        public decimal? Humidity { get; set; }
+        public decimal? WindSpeed { get; set; }
+        public string WeatherCondition { get; set; }
+    }
+}
