e98a6e0947435baedb4931c1ed3979d45d6a0cf1
jprocto3 <jprocto3@mix.wvu.edu>
2024-03-27 17:09:21 -0400

    API???

---
+++ MIST_353_Group_API/Controllers/WeatherController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MIST_353_Group_API.Entities;
+using MIST_353_Group_API.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MIST_353_Group_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        private readonly IWeatherService weatherService;
+
+        public WeatherController(IWeatherService weatherService)
+        {
+            this.weatherService = weatherService;
+        }
+
+        [HttpGet("{WeatherID}")]
+        public async Task<IActionResult> CarterProctorSPs(int WeatherID)
+        {
+            var humidityDetails = await weatherService.CarterProctorSPs(WeatherID);
+
+            if (humidityDetails == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(humidityDetails);
+        }
+    }
+}
