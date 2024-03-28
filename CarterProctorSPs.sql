150ee6208feaaf7ce1859055436096845b30aad5
Carter Proctor <142524823+tictactacoooo@users.noreply.github.com>
2024-03-28 18:58:37 -0400

    Update CarterProctorSPs.sql

--- CarterProctorSPs.sql
+++ CarterProctorSPs.sql
@@ -1,30 +1,14 @@
-CREATE PROCEDURE CarterProctorSPs --Get Park Status
+CREATE PROCEDURE CarterProctorSPs --GetParkName
     @LocationID INT
 AS
 BEGIN
-    DECLARE @WeatherCondition NVARCHAR(50)
-    DECLARE @ActiveFireWarnings INT
+    DECLARE @ParkName NVARCHAR(100);
 
-    -- Get the current weather condition for the park
-    SELECT @WeatherCondition = [WeatherCondition]
-    FROM [dbo].[Weather]
+    -- Get the park name for the given location
+    SELECT @ParkName = [ParkName]
+    FROM [dbo].[Park]
     WHERE [LocationID] = @LocationID;
 
-    -- Check for active fire warnings for the park
-    SELECT @ActiveFireWarnings = COUNT(*)
-    FROM [dbo].[Fire_Warning]
-    WHERE [LocationID] = @LocationID
-    AND [Status] = 'Active';
-
-    -- Determine park status based on weather and fire warnings
-    IF (@WeatherCondition = 'Extreme' OR @ActiveFireWarnings > 0)
-    BEGIN
-        -- Park is closed due to extreme weather or active fire warnings
-        SELECT 'Closed' AS ParkStatus;
-    END
-    ELSE
-    BEGIN
-        -- Park is open
-        SELECT 'Open' AS ParkStatus;
-    END
+    -- Return the park name
+    SELECT @ParkName AS ParkName;
 END;
