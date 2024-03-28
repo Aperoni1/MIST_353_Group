c4fc1cdcc8d858a6368a80ad2c9ac91e651d8e27
jprocto3 <jprocto3@mix.wvu.edu>
2024-03-28 15:09:59 -0400

    data

--- MIST_353_Group_API/Data/DbContextClass.cs
+++ MIST_353_Group_API/Data/DbContextClass.cs
@@ -6,14 +6,10 @@ namespace MIST_353_Group_API.Data
 {
     public class DbContextClass : DbContext
     {
-        protected readonly IConfiguration Configuration;
-        public DbContextClass(IConfiguration configuration)
+        
+       public DbContextClass(DbContextOptions<DbContextClass> options) : base(options)
         {
-            Configuration = configuration;
-        }
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-        {
-            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+
         }
         public DbSet<FireWarning> FireWarning {get; set;}
         public DbSet<Location> Location { get; set;}
