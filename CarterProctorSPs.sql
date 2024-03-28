da592c6b72359b554d244dd9373d89d9427aec46
Carter Proctor <142524823+tictactacoooo@users.noreply.github.com>
2024-03-28 11:43:24 -0400

    Update CarterProctorSPs.sql

--- CarterProctorSPs.sql
+++ CarterProctorSPs.sql
@@ -1,14 +1,30 @@
-CREATE PROCEDURE GetHumidityByLocation
+CREATE PROCEDURE GetParkStatus
     @LocationID INT
 AS
 BEGIN
-    DECLARE @Humidity DECIMAL(5, 2)
-    SELECT 
-        @Humidity = [Humidity]
-    FROM 
-        [dbo].[Weather]
-    WHERE 
-        [LocationID] = @LocationID;
+    DECLARE @WeatherCondition NVARCHAR(50)
+    DECLARE @ActiveFireWarnings INT
 
-    SELECT @Humidity AS Humidity;
+    -- Get the current weather condition for the park
+    SELECT @WeatherCondition = [WeatherCondition]
+    FROM [dbo].[Weather]
+    WHERE [LocationID] = @LocationID;
+
+    -- Check for active fire warnings for the park
+    SELECT @ActiveFireWarnings = COUNT(*)
+    FROM [dbo].[Fire_Warning]
+    WHERE [LocationID] = @LocationID
+    AND [Status] = 'Active';
+
+    -- Determine park status based on weather and fire warnings
+    IF (@WeatherCondition = 'Extreme' OR @ActiveFireWarnings > 0)
+    BEGIN
+        -- Park is closed due to extreme weather or active fire warnings
+        SELECT 'Closed' AS ParkStatus;
+    END
+    ELSE
+    BEGIN
+        -- Park is open
+        SELECT 'Open' AS ParkStatus;
+    END
 END;
