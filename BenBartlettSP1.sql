3c57942d20447b2f6a1fba26c810c203d76862dc
BenBart003 <142524919+BenBart003@users.noreply.github.com>
2024-04-02 22:08:33 -0400

    Update BenBartlettSP1.sql
    fixes

--- BenBartlettSP1.sql
+++ BenBartlettSP1.sql
@@ -1,25 +1,33 @@
 CREATE PROC spGetWeatherByState
-	@State NVARCHAR(50)
+    @State NVARCHAR(50)
 AS
 BEGIN
     DECLARE @Temperature DECIMAL(10, 2)
     DECLARE @Humidity DECIMAL(5, 2)
     DECLARE @WindSpeed DECIMAL(10, 2)
     DECLARE @WeatherCondition NVARCHAR(100)
+    DECLARE @WeatherID INT
+    DECLARE @LocationID INT 
 
-	    SELECT 
-        @Temperature = [Temperature],
-        @Humidity = [Humidity],
-        @WindSpeed = [WindSpeed],
-        @WeatherCondition = [WeatherCondition]
+    
+    SELECT TOP 1
+        @Temperature = Weather.Temperature,
+        @Humidity = Weather.Humidity,
+        @WindSpeed = Weather.WindSpeed,
+        @WeatherCondition = Weather.WeatherCondition,
+        @WeatherID = Weather.WeatherID,
+        @LocationID = Location.LocationID 
     FROM 
-        [dbo].[Weather]
+        dbo.Weather
+        INNER JOIN dbo.Location ON Weather.LocationID = Location.LocationID
     WHERE 
-		[State] = @State;
+        Location.State = @State; 
 
-	SELECT 
+    SELECT 
+        @WeatherID AS WeatherID,
         @Temperature AS Temperature,
         @Humidity AS Humidity,
         @WindSpeed AS WindSpeed,
-        @WeatherCondition AS WeatherCondition;
+        @WeatherCondition AS WeatherCondition,
+        @LocationID AS LocationID; 
 END;
