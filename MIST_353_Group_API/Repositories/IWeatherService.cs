766c0cec85c9591f8f3267d3171b9ce9c5874fe3
jprocto3 <jprocto3@mix.wvu.edu>
2024-04-05 10:18:18 -0400

    update for sql error in weather services

--- MIST_353_Group_API/Repositories/IWeatherService.cs
+++ MIST_353_Group_API/Repositories/IWeatherService.cs
@@ -1,15 +1,11 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using MIST_353_Group_API.Entities;
-
 
 namespace MIST_353_Group_API.Repositories
 {
     public interface IWeatherService
     {
-        Task<Weather> GetWeatherByParkName(string parkName); // Weather by Park Name SP
-
-        Task<Weather> GetWeatherByLocation(int locationId); // Weather by Location ID SP
+        Task<Weather> GetWeatherByParkName(string parkName);
+        Task<Weather> GetWeatherByLocation(int locationId);
     }
 }
-
--- MIST_353_Group_API/Repositories/WeatherService.cs
+++ MIST_353_Group_API/Repositories/WeatherService.cs
@@ -1,6 +1,9 @@
 ﻿using MIST_353_Group_API.Entities;
 using Microsoft.EntityFrameworkCore;
 using MIST_353_Group_API.Data;
+using Microsoft.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MIST_353_Group_API.Repositories
 {
@@ -15,11 +18,13 @@ namespace MIST_353_Group_API.Repositories
 
         public async Task<Weather> GetWeatherByParkName(string parkName)
         {
-            var result = await _dbContextClass.Weather
-                .FromSqlInterpolated($"EXEC CarterProctorSPs @ParkName={parkName}")
-                .FirstOrDefaultAsync();
+            var ParkWeather =  _dbContextClass.Weather
+                .FromSqlRaw("EXEC CarterProctorSPs @parkName",
+                            new SqlParameter("@parkName", parkName))
+                .AsEnumerable();
+            var parkWeatherByName = ParkWeather.FirstOrDefault();
 
-            return result;
+            return parkWeatherByName;
         }
 
         public async Task<Weather> GetWeatherByLocation(int locationId)
