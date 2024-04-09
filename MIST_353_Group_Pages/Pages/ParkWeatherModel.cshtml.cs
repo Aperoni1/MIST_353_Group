a66f7b44f0ddb82a6a56a1de3de904c91683fabb
jprocto3 <jprocto3@mix.wvu.edu>
2024-04-09 17:37:21 -0400

    Park Weather and Model

---
+++ MIST_353_Group_Pages/Pages/ParkWeatherModel.cshtml.cs
@@ -0,0 +1,19 @@
+using DocumentFormat.OpenXml.Bibliography;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Reflection.Emit;
+using System.Xml.Linq;
+
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MIST_353_Group_Pages.Pages
+{
+    public class ParkWeatherModel : PageModel
+    {
+        public string ParkName { get; set; }
+
+        public void OnGet()
+        {
+        }
+    }
+}
